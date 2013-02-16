ef906f815c82bc9c6e1d31e73fc3ddb527298ec9
myun2 <myun2@nwhite.info>
2013-02-16 16:34:01 +0900

    initial commit
    * README.md
    * flax.hpp

---
+++ include/myun2/flax.hpp
@@ -0,0 +1,6 @@
+#ifndef __github_com_myun2__flax_HPP__
+#define __github_com_myun2__flax_HPP__
+
+#include "myun2/flax/flax.hpp"
+
+#endif//__github_com_myun2__flax_HPP__
---
+++ include/myun2/flax/flax.hpp
@@ -0,0 +1,4 @@
+#ifndef __github_com_myun2__flax__flax_HPP__
+#define __github_com_myun2__flax__flax_HPP__
+
+#endif//__github_com_myun2__flax__flax_HPP__
