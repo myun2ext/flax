3c0603f75763ea9fb35c320ed0a157af9bd8e5c9
myun2 <myun2@nwhite.info>
2013-03-05 12:03:47 +0900

    File Open, And Open Error make.bat Fix

--- flax2.cpp
+++ flax2.cpp
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
+int flax_readfile(const char* filename);
+
 int main(int argc, char *argv[])
 {
 	if ( argc == 1 ) {
-		printf("usage: flax <Compile File Name>\n");
+		fprintf(stderr, "usage: flax <Compile File Name>\n");
+		return 1;
+	}
+
+	const char* flax_file = argv[1];
+
+	return flax_readfile(flax_file);
+}
+
+int flax_readfile(const char* filename)
+{
+	FILE* fp = fopen(filename, "r");
+	if ( fp == NULL ) {
+		fprintf(stderr, "flax File Open Error. %s\n", filename);
 		return 1;
 	}
 
--- make.bat
+++ make.bat
@@ -1,9 +1,11 @@
 @echo off
 set CC=mingw32-c++
 set CFLAGS=-O3
-set OUT=flax2.exe
+set OUTFILE=flax2.exe
 
 @echo on
 
-%CC% %INCLUDES% %CFLAGS% flax2.cpp -o flax2.exe
+%CC% %INCLUDES% %CFLAGS% flax2.cpp -o %OUTFILE%
+
+%OUTFILE%
 pause
