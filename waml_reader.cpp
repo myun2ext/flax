ec7eacff790a8824e037a6df432f984a8a1e850e
myun2 <myun2@nwhite.info>
2013-03-05 13:10:01 +0900

    WamlLine

--- waml_reader.cpp
+++ waml_reader.cpp
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <string>
 
+class WamlLine
+{
+};
+
 class WamlDocument
 {
 };
 
 ////////////////////////////////
-int waml_line(const char* line)
+WamlLine waml_line(const char* line)
 {
 	printf("%s\n");
 	return 0;
 }
 
-int waml_read(const char* p)
+WamlDocument waml_read(const char* p)
 {
+	WamlDocument doc;
 	::std::string line;
 	for(; *p; p++)
 	{
 		if ( *p == '\n' )
-			flax_line(line.c_str());
+			doc += flax_line(line.c_str());
 		else
 			line += *p;
 	}
-	return 0;
+	return doc;
 }
