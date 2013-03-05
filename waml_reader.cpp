1b5bbfcb8d637737dcbfdc371be584816f46146b
myun2 <myun2@nwhite.info>
2013-03-05 13:53:16 +0900

    return sub_documents()

--- waml_reader.cpp
+++ waml_reader.cpp
@@ -47,6 +47,8 @@ class WamlDocument
 private:
 	::std::vector<WamlSubDocument> m_sub_documents;
 public:
+	WamlSubDocument sub_documents() { return m_sub_documents; }
+	const WamlSubDocument sub_documents() const { return m_sub_documents; }
 };
 
 ////////////////////////////////
