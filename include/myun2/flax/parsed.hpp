689222416fe2ab2af8529009902d2a34ed2121b5
myun2 <myun2@nwhite.info>
2013-04-07 01:58:49 +0900

    parsed.hpp Add

---
+++ include/myun2/flax/parsed.hpp
@@ -0,0 +1,11 @@
+#ifndef __github_com_myun2__flax__parsed_HPP__
+#define __github_com_myun2__flax__parsed_HPP__
+
+namespace myun2
+{
+	namespace flax
+	{
+	}
+}
+
+#endif//__github_com_myun2__flax__parsed_HPP__
