1fc8f230d5bc4e6b41881db53e5f1620f822b949
myun2 <myun2@nwhite.info>
2013-04-07 01:57:27 +0900

    include/myun2/flax/runner.hpp Add

---
+++ include/myun2/flax/runner.hpp
@@ -0,0 +1,13 @@
+#ifndef __github_com_myun2__flax__runner_HPP__
+#define __github_com_myun2__flax__runner_HPP__
+
+namespace myun2
+{
+	namespace flax
+	{
+		void run()
+		{}
+	}
+}
+
+#endif//__github_com_myun2__flax__runner_HPP__
