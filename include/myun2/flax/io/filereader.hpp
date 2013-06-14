ee44428645797b94546465fbfc34bfdbcc96e365
myun2 <myun2@nwhite.info>
2013-06-15 03:15:44 +0900

    filereader.hpp, filewriter.hpp Add

---
+++ include/myun2/flax/io/filereader.hpp
@@ -0,0 +1,32 @@
+#ifndef __github_com_myun2__flax__io__filereader_HPP__
+#define __github_com_myun2__flax__io__filereader_HPP__
+
+#include <stdio.h>
+
+namespace myun2
+{
+	namespace flax
+	{
+		namespace io
+		{
+			struct filereader_open_error {};
+			
+			class filereader
+			{
+			private:
+				FILE* fp;
+			public:
+				filereader(const char* path, bool binary=true) {
+					fp = fopen(path);
+					if ( fp == NULL )
+						throw filereader_open_error();
+				}
+				typedef int T;
+				int read() { return getc(fp); }
+				bool is_end(int c) const { return c == EOF; }
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__flax__io__filereader_HPP__
---
+++ include/myun2/flax/io/filewriter.hpp
@@ -0,0 +1,31 @@
+#ifndef __github_com_myun2__flax__io__filewriter_HPP__
+#define __github_com_myun2__flax__io__filewriter_HPP__
+
+#include <stdio.h>
+
+namespace myun2
+{
+	namespace flax
+	{
+		namespace io
+		{
+			struct filewriter_open_error {};
+			
+			class filewriter
+			{
+			private:
+				FILE* fp;
+			public:
+				filereader(const char* path, bool binary=true) {
+					fp = fopen(path);
+					if ( fp == NULL )
+						throw filewriter_open_error();
+				}
+				typedef int T;
+				void write(int c) { putchar(c); }
+			};
+		}
+	}
+}
+
+#endif//__github_com_myun2__flax__io__stdoutput_HPP__
