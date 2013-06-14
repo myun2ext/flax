a73fcb1f1ab19fc1055ee05d295a26404aa962cd
myun2 <myun2@nwhite.info>
2013-06-14 23:54:51 +0900

    stack_machine.hpp Add

---
+++ include/myun2/flax/stack_machine.hpp
@@ -0,0 +1,15 @@
+#ifndef __github_com_myun2__flax__stack_machine_HPP__
+#define __github_com_myun2__flax__stack_machine_HPP__
+
+namespace myun2
+{
+	namespace flax
+	{
+		class stack_machine
+		{
+		public:
+		};
+	}
+}
+
+#endif//__github_com_myun2__flax__stack_machine_HPP__
