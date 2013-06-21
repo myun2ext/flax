147cfca54e51b38b1ae8242f4d3b35b6c5cdbb98
myun2 <myun2@nwhite.info>
2013-06-21 11:57:36 +0900

    add exception, standard_error.hpp

---
+++ include/myun2/flax/base/exception.hpp
@@ -0,0 +1,12 @@
+#ifndef __github_com_myun2__flax__exception_HPP__
+#define __github_com_myun2__flax__exception_HPP__
+
+namespace myun2
+{
+	namespace flax
+	{
+		struct exception {};
+	}
+}
+
+#endif//__github_com_myun2__flax__exception_HPP__
---
+++ include/myun2/flax/base/standard_error.hpp
@@ -0,0 +1,14 @@
+#ifndef __github_com_myun2__flax__standard_error_HPP__
+#define __github_com_myun2__flax__standard_error_HPP__
+
+#include "myun2/flax/base/exception.hpp"
+
+namespace myun2
+{
+	namespace flax
+	{
+		struct standard_error : exception {};
+	}
+}
+
+#endif//__github_com_myun2__flax__standard_error_HPP__
