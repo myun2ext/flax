80ac3659f917c4d956e78ab58f63c6ba86b022b9
myun2 <myun2@nwhite.info>
2013-06-25 13:21:12 +0900

    line_writer.hpp Update

--- include/myun2/flax/util/line_writer.hpp
+++ include/myun2/flax/util/line_writer.hpp
@@ -12,18 +12,19 @@ namespace myun2
 			template <typename _Writer>
 			class line_writer : public _Writer
 			{
-			private:
-				FILE* fp;
 			public:
 				line_writer(){}
 				line_writer(const &_Writer w) : _Writer(w){}
 
 				typedef const char* T;
 				void write(const char* s) {
+					while(*s) {
+						_Writer::write(*s++);
+					}
 				}
 			};
 		}
 	}
 }
 
-#endif//__github_com_myun2__flax__io__stdoutput_HPP__
+#endif//__github_com_myun2__flax__io__line_writer_HPP__
